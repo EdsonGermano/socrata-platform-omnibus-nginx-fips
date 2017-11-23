9bc6bdce1562d9425c5cc0cc6c84dff633e9ceca
Jonathan Hartman <j@hartman.io>
2017-11-22 16:19:43 -0800

    Add tests to ensure OpenSSL is operating in FIPS mode by default

--- test/integration/default/app_test.rb
+++ test/integration/default/app_test.rb
@@ -9,3 +9,15 @@ describe directory('/opt/nginx') do
   its(:group) { should eq('root') }
   its(:mode) { should cmp('0755') }
 end
+
+describe command('openssl md5 /etc/passwd') do
+  its(:exit_status) { should eq(0) }
+  its(:stdout) { should include('MD5(/etc/passwd)=') }
+end
+
+describe command('/opt/nginx/embedded/bin/openssl md5 /etc/passwd') do
+  its(:exit_status) { should eq(1) }
+  its(:stderr) do
+    should include('routines:FIPS_DIGESTINIT:disabled for fips:fips_md.c:')
+  end
+end
