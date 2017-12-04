5e8f633323c3afd0b23f3263c7dabe5f38bbbcfa
Jonathan Hartman <j@hartman.io>
2017-12-04 11:15:38 -0800

    Build in all the modules that the Ubuntu package comes with

--- omnibus/config/software/nginx.rb
+++ omnibus/config/software/nginx.rb
@@ -50,16 +50,42 @@ build do
     '--lock-path=/var/lock/nginx.lock',
     '--http-log-path=/var/log/nginx/access.log',
     '--error-log-path=/var/log/nginx/error.log',
-    '--http-client-body-temp-path=/var/cache/nginx/body',
-    '--http-fastcgi-temp-path=/var/cache/nginx/fastcgi',
-    '--http-proxy-temp-path=/var/cache/nginx/proxy',
-    '--http-scgi-temp-path=/var/cache/nginx/scgi',
-    '--http-uwsgi-temp-path=/var/cache/nginx/uwsgi'
-    '--with-ipv6',
+    '--http-client-body-temp-path=/var/cache/nginx/client_temp',
+    '--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp',
+    '--http-proxy-temp-path=/var/cache/nginx/proxy_temp',
+    '--http-scgi-temp-path=/var/cache/nginx/scgi_temp',
+    '--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp',
+    '--with-threads',
     '--with-debug',
+    '--with-compat',
+    '--with-file-aio',
+    '--with-http_addition_module',
+    '--with-http_auth_request_module',
+    '--with-http_dav_module',
+    '--with-http_flv_module',
+    '--with-http_gunzip_module',
+    '--with-http_gzip_static_module',
+    '--with-http_mp4_module',
+    '--with-http_random_index_module',
+    '--with-http_realip_module',
+    '--with-http_secure_link_module',
+    '--with-http_slice_module',
     '--with-http_ssl_module',
-    '--with-http_stub_status_module'
+    '--with-http_stub_status_module',
+    '--with-http_sub_module',
+    '--with-http_v2_module',
+    '--with-mail',
+    '--with-mail_ssl_module',
+    '--with-stream',
+    '--with-stream_realip_module',
+    '--with-stream_ssl_module',
+    '--with-stream_ssl_preread_module'
   ].join(' '), env: env
+
+  # TODO: What about these other configure flags?
+  # --modules-path=/usr/lib/nginx/modules
+  # --user=nginx
+  # --group=nginx
 
   make "-j #{workers}", env: env
   make 'install', env: env
--- test/integration/default/app_test.rb
+++ test/integration/default/app_test.rb
@@ -4,6 +4,38 @@ describe package('nginx') do
   it { should be_installed }
 end
 
+describe command('/usr/sbin/nginx -V') do
+  %w[
+    --with-threads
+    --with-debug
+    --with-compat
+    --with-file-aio
+    --with-http_addition_module
+    --with-http_auth_request_module
+    --with-http_dav_module
+    --with-http_flv_module
+    --with-http_gunzip_module
+    --with-http_gzip_static_module
+    --with-http_mp4_module
+    --with-http_random_index_module
+    --with-http_realip_module
+    --with-http_secure_link_module
+    --with-http_slice_module
+    --with-http_ssl_module
+    --with-http_stub_status_module
+    --with-http_sub_module
+    --with-http_v2_module
+    --with-mail
+    --with-mail_ssl_module
+    --with-stream
+    --with-stream_realip_module
+    --with-stream_ssl_module
+    --with-stream_ssl_preread_module
+  ].each do |flag|
+    its(:stdout) { should include(flag) }
+  end
+end
+
 %w[/opt/nginx /var/log/nginx /var/cache/nginx].each do |d|
   describe directory(d) do
     its(:owner) { should eq('root') }
